63d87b92c571b05c78ad46446a755d5df649e175
Gabriel Jesus de Sena <62814455+Oranj1@users.noreply.github.com>
2020-03-28 18:22:57 -0300

    Create 1012.c

---
+++ 1012.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    
+    double A;
+    double B;
+    double C;
+
+    scanf("%lf",&A);
+    scanf("%lf",&B);
+    scanf("%lf",&C);
+
+    printf("TRIANGULO: %.3lf\n", (A*C)/2);
+    printf("CIRCULO: %.3lf\n", (C*C)*3.14159);
+    printf("TRAPEZIO: %.3lf\n", ((A+B)*C)/2);
+    printf("QUADRADO: %.3lf\n", B*B);
+    printf("RETANGULO: %.3lf\n", (A*B));
+    return 0;
+}
