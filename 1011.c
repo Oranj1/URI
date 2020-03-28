f23aba9e78f0e8f90c3aa97b53740e9ea13b9997
Gabriel Jesus de Sena <62814455+Oranj1@users.noreply.github.com>
2020-03-28 18:19:45 -0300

    Create  1011.c

---
+++ 1011.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+        double raio;
+        double pi = 3.14159;
+
+        scanf("%lf",&raio);
+
+        printf("VOLUME = %.3f\n",(4.0/3.0) * pi * (raio*raio*raio));
+
+    return 0;
+}
