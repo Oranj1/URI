6c8bd6aa1c90d08b2a7a4d40a452a2b7e886ed36
Gabriel Jesus de Sena <62814455+Oranj1@users.noreply.github.com>
2020-03-28 18:17:41 -0300

    Create  1759.cs

---
+++ 1759.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+            int times = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; times - 1 > i; i++)
+            {
+                Console.Write("Ho ");
+            }
+            Console.WriteLine("Ho!");
+    }
+}
